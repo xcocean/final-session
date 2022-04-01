f2f225936188451803a0b019ace272b930c7712a
lingkang <lingkang@agree.com.cn>
2022-04-01 15:08:45 +0800

    将数据库存储的blob扩展为longblob

--- final-session-core/src/main/resources/sql/mysql.sql
+++ final-session-core/src/main/resources/sql/mysql.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `fs_session`
 (
     `id`          varchar(64) NOT NULL,
-    `session`     blob,
+    `session`     longblob,
     `update_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
     `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`)
